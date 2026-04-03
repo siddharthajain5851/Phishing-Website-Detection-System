564d014b16cf6c7fec0354bf70bf0c891e30d4bf
siddharthajain5851 <siddharthajain5851@gmail.com>
2026-04-03 12:55:59 +0530

    Implement /predict API for URL phishing detection
    Added a new API endpoint for URL prediction.

--- final project/app.py
+++ final project/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+ from flask import Flask, render_template, request ,jsonify
 import pickle
 import re
 
@@ -25,5 +25,23 @@ def index():
 
     return render_template("index.html", prediction=prediction)
 
+@app.route("/predict", methods=["POST"])
+def predict_api():
+    data = request.get_json()
+    url = data.get("url")
+
+    cleaned_url = re.sub(r"^https?://(www\.)?", "", url)
+    result = model.predict(vectorizer.transform([cleaned_url]))[0]
+
+    if result == "bad":
+        output = "Phishing"
+    else:
+        output = "Safe"
+
+    return jsonify({
+        "url": url,
+        "prediction": output
+    })
 if __name__ == "__main__":
     app.run(host="0.0.0.0" , port=10000)
+     
